1a4e629ed56ec35a75a28bb2f36c068e23f039b5
KhyatiMehta3 <khyatimehta69@gmail.com>
2018-03-06 19:47:23 +0530

    Update image.py

--- RCTrainingData/image.py
+++ RCTrainingData/image.py
@@ -5,5 +5,5 @@ import cv2
 def save_image_with_direction(stream, direction):
     """Save image"""
     stream.seek(0)
-    image = cv2.imread(stream)
+    image = cv2.imdecode(stream,0)
     image.cv2.imwrite('image%s.jpg' % ("-" + direction + "-"+ str(time.time())), format="JPEG")
