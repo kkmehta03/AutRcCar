6d6fa0f17a07a6bf9481e24f528067f0155960ec
KhyatiMehta3 <khyatimehta69@gmail.com>
2018-03-20 17:20:44 +0530

    Add files via upload

---
+++ gpioController.py
@@ -0,0 +1,29 @@
+def ControllerInit():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(03, GPIO.OUT) #back motor pin
+    GPIO.setup(05, GPIO.OUT) #back motor pin
+    GPIO.setup(07, GPIO.OUT) #left pin
+    GPIO.setup(11, GPIO.OUT) #right pin
+
+def forwardGPIO():
+    GPIO.output(03,True)
+    GPIO.output(05,False)
+    clean()
+
+def reverseGPIO():
+    GPIO.output(03,False)
+    GPIO.output(05,True)
+    clean()
+
+def rightGPIO():
+    GPIO.output(07,False)
+    GPIO.output(11,True)
+    clean()
+
+def leftGPIO():
+    GPIO.output(07,True)
+    GPIO.output(11,False)
+    clean()
+
+def clean():
+    GPIO.cleanup()
