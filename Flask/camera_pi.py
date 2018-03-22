10b5a97545caf5cb2a993bcd95369d98b8dc4984
KhyatiMehta3 <khyatimehta69@gmail.com>
2018-03-22 23:34:59 +0530

    Create camera_pi.py

---
+++ Flask/camera_pi.py
@@ -0,0 +1,28 @@
+"""
+credits: Miguel Grinberg
+Github: github.com/miguelgrinberg/flask-video-streaming
+"""
+
+import io
+import time
+import picamera
+from base_camera import BaseCamera
+
+
+class Camera(BaseCamera):
+    @staticmethod
+    def frames():
+        with picamera.PiCamera() as camera:
+            # let camera warm up
+            time.sleep(2)
+
+            stream = io.BytesIO()
+            for _ in camera.capture_continuous(stream, 'jpeg',
+                                                 use_video_port=True):
+                # return current frame
+                stream.seek(0)
+                yield stream.read()
+
+                # reset stream for next frame
+                stream.seek(0)
+                stream.truncate()
