ee8747a5d6b7f1464982db69773d3ea0121c6cbf
KhyatiMehta3 <khyatimehta69@gmail.com>
2018-03-23 13:49:11 +0530

    Update gpioController.py

--- Flask/gpioController.py
+++ Flask/gpioController.py
@@ -1,30 +1,35 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(03, GPIO.OUT) #back motor pin
-GPIO.setup(05, GPIO.OUT) #back motor pin
-GPIO.setup(07, GPIO.OUT) #left pin
-GPIO.setup(11, GPIO.OUT) #right pin
+def ControllerInit():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(03, GPIO.OUT) #back motor pin
+    GPIO.setup(05, GPIO.OUT) #back motor pin
+    GPIO.setup(07, GPIO.OUT) #left pin
+    GPIO.setup(11, GPIO.OUT) #right pin
 
 def forwardGPIO():
+    ControllerInit()
     GPIO.output(03,True)
     GPIO.output(05,False)
     
 def reverseGPIO():
+    ControllerInit()
     GPIO.output(03,False)
     GPIO.output(05,True)
     
 def rightGPIO():
+    ControllerInit()
     GPIO.output(07,False)
     GPIO.output(11,True)
     
 def leftGPIO():
+    ControllerInit()
     GPIO.output(07,True)
     GPIO.output(11,False)
 def stopGPIO():
-    GPIO.output(03,True)
-    GPIO.output(05,True)
-    GPIO.output(07,True)
-    GPIO.output(11,True)
+    GPIO.output(03,False)
+    GPIO.output(05,False)
+    GPIO.output(07,False)
+    GPIO.output(11,False)
     
 def clean():
     GPIO.cleanup()
