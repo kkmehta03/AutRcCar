c0a1a540eb4b69cfb078b6a013bbdd11649ee21b
KhyatiMehta3 <khyatimehta69@gmail.com>
2018-04-14 14:57:01 +0530

    Update Autodriver.py

--- Flask/Autodriver.py
+++ Flask/Autodriver.py
@@ -11,8 +11,9 @@ layer_size = np.int32([38400,4,2,8,8,4])
 model.create(layer_size)
 model.load('autrccar/Flask/mlp_xml/mlp.xml')
 
-def predict(samples):
+def predictor(samples):
   ret, resp = model.predict(samples)
+  print(resp.argmax(-1))
   return resp.argmax(-1)
 
 def steer(prediction):
@@ -24,24 +25,16 @@ def steer(prediction):
     g.rightGPIO()
   else:
     g.stopGPIO()
-    
+
 with picamera.PiCamera() as cam:
   cam.resolution = (320,240)
   cam.framerate = 10
-  cam.start_preview()
-  time.sleep(2)
+  start = time.time()
   stream = io.BytesIO()
   for foo in cam.capture_continuous(stream,'jpeg',use_video_port=True):
-      jpg=np.empty((240*320*3),dtype=np.uint8)
-      print(type(jpg))
-      cam.capture(jpg,'bgr')
-      image = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_GRAYSCALE)
-      roi = image[120:240,:]
-      cv2.imshow('image',image)
-      image_array = roi.reshape(1,38400).astype(np.float32)
-      prediction = model.predict(image_array)
-      steer(prediction)
-      
-      
-      
+    stream.seek(0)
+    jpg = stream.read()
+    gray = cv2.imdecode(np.fromstring(jpg,dtype=np.uint8),cv2.IMREAD_GRAYSCALE)
+    roi = gray[120:240, :]
+    image_array = roi.reshape(1,38400).astype(np.float32)
     
