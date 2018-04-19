ffbc07c629b9ca218fdba8353a9202a02a1a6748
KhyatiMehta3 <khyatimehta69@gmail.com>
2018-04-19 19:27:18 +0530

    Update Autodriver.py

--- Flask/Autodriver.py
+++ Flask/Autodriver.py
@@ -6,14 +6,17 @@ import gpioController as g
 import io
 
 class neuralnet(object):
-  def __init__(self,file_path):
-    self.model = cv2.ml.ANN_MLP_load(file_path)
+  def __init__(self,filepath):
+    self.model = cv2.ml.ANN_MLP_load(filepath)
   #def create(self):
     #layer_size = np.int32([38400,4,2,8,8,4])
     #self.model.create(layer_size)
     #self.model.load('mlp_xml/mlp.xml')
   def predict(self, samples):
     ret, resp = self.model.predict(samples)
+    print(ret)
+    print(resp)
+    print(resp.argmax(-1))
     return resp.argmax(-1)
 
 #model = cv2.ANN_MLP()
@@ -28,15 +31,18 @@ class neuralnet(object):
 
 def steer(prediction):
   if prediction == 2:
+    print('forward')
     g.forwardGPIO()
   elif prediction == 0:
+    print('left')
     g.leftGPIO()
   elif prediction == 1:
+    print('right')
     g.rightGPIO()
   else:
     g.stopGPIO()
 
-model = neuralnet('/autrccar/Flask/mlp.xml')
+model = neuralnet('mlp.xml')
 with picamera.PiCamera() as cam:
   cam.resolution = (320,240)
   cam.framerate = 10
@@ -48,6 +54,7 @@ with picamera.PiCamera() as cam:
     gray = cv2.imdecode(np.fromstring(jpg,dtype=np.uint8),cv2.IMREAD_GRAYSCALE)
     roi = gray[120:240, :]
     image_array = roi.reshape(1,38400).astype(np.float32)
+    print(image_array)
     print(type(image_array))
     print(image_array.size)
     print(image_array.shape)
