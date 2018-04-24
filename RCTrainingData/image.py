fed37f97c5d7b68f89bc7f20a1084db278cc1ef6
KhyatiMehta3 <khyatimehta69@gmail.com>
2018-04-24 19:13:19 +0530

    Delete image.py

--- RCTrainingData/image.py
+++
@@ -1,9 +0,0 @@
-"""Image helpers"""
-import time
-import cv2
-
-def save_image_with_direction(stream, direction):
-    """Save image"""
-    stream.seek(0)
-    image = cv2.imdecode(stream,0)
-    image.cv2.imwrite('image%s.jpg' % ("-" + direction + "-"+ str(time.time())), format="JPEG")
