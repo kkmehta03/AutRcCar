bbd01335011594ae251c8ac1d36d8cc52141eb01
KhyatiMehta3 <khyatimehta69@gmail.com>
2018-04-27 19:13:37 +0530

    Update Autodriver.py

--- Flask/Autodriver.py
+++ Flask/Autodriver.py
@@ -7,49 +7,33 @@ import cv2
 import gpioController as g
 import io
 import us1
-
 class neuralnet(object):
   def __init__(self,filepath):
     self.model = cv2.ml.ANN_MLP_load(filepath)
-  #def create(self):
-    #layer_size = np.int32([38400,4,2,8,8,4])
-    #self.model.create(layer_size)
-    #self.model.load('mlp_xml/mlp.xml')
   def predict(self, samples):
     ret, resp = self.model.predict(samples)
     print(ret)
     print(resp)
     print(resp.argmax(-1))
     return resp.argmax(-1)
-
-#model = cv2.ANN_MLP()
-#layer_size = ([38400,32,4])
-#model.create(layer_size)
-#model.load('/autrccar/Flask/mlp_xml/mlp.xml')
-
-#def predictor(samples):
-  #ret, resp = model.predict(samples)
-  #print(ret.argmax(-1))
-  #return (ret.argmax(-1))
-
 def steer(prediction):
   dist = us1.distance()
   print(dist)
-  if dist > 3000 and dist < 4000:
-    g.reverseGPIO()
-    g.reverseGPIO()
-  elif prediction == 1 and dist > 40:
+  if prediction == 1 and dist > 40:
     print('forward')
     g.forwardGPIO()
   elif dist < 40 and prediction == 1:
     print('too close! going away')
     g.reverseGPIO(1.6)
+    time.sleep(0.1)
+    g.leftGPIO()
   elif prediction == 0 and dist > 40 :
     print('left')
     g.leftGPIO()
   elif dist < 40 and prediction == 0:
     print('nope! going right!')
-    g.reverseGPIO()
+    g.reverseGPIO(1.6)
+    time.sleep(0.1)
     g.rightGPIO()
   elif prediction == 2 and dist > 40:
     print('right')
@@ -57,12 +41,13 @@ def steer(prediction):
   elif dist < 40 and prediction == 2:
     print('nope! going left')
     g.reverseGPIO(1.6)
+    time.slee(0.1)
     g.leftGPIO()
   else:
     print('Take care of me please :)')
     g.stopGPIO()
-    
-model = neuralnet('mlp1.xml')
+
+model = neuralnet('mlp.xml')
 with picamera.PiCamera() as cam:
   cam.resolution = (320,240)
   cam.framerate = 10
@@ -71,15 +56,12 @@ with picamera.PiCamera() as cam:
   for foo in cam.capture_continuous(stream,'jpeg',use_video_port=True):
     stream.seek(0)
     jpg = stream.read()
-    gray = cv2.imdecode(np.fromstring(jpg,dtype=np.uint8),cv2.IMREAD_GRAYSCALE)
-    roi = gray[120:240, :]
-    #cv2.imshow('image',roi)
-    image_array = roi.reshape(1,38400).astype(np.float32)
+    color_image = cv2.imdecode(np.fromstring(jpg,dtype=np.uint8),cv2.IMREAD_COLOR)
+    roi = color_image[120:240, :]
+    image_array = roi.reshape(1,115200).astype(np.float32)
     print(image_array)
-    #print(type(image_array))
-    #print(image_array.size)
-    #print(image_array.shape)
-    #prediction = predictor(image_array)
+    print(image_array.size)
+    print(image_array.shape)
     prediction = model.predict(image_array)
     steer(prediction)
     stream.seek(0)
